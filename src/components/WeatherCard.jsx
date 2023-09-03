aa9e18ea3b6165c3b50e7e3aa0bd91c5202eea41
ADCristancho <ali3184533013@gmail.com>
2023-09-03 14:27:38 -0500

    change5

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -11,29 +11,38 @@ const WeatherCard = ({weather, temp}) => {
   
     return (
       <article className="contenido">
+        <div className="contenidotitulo">
           <h1>Weather Apps</h1>
+
+        </div>
+
+        <div className="contenidotemp">
+
+          <img className="clouds"
+          src={weather && `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`}/>
+          <h2>{isCelsius ? `${temp?.celsius} ℃`: `${temp?.farenheit} ℉`}</h2>
+        </div>
+        <div className="contenidoboton">
+          <button onClick={handleChangeTemp}>{isCelsius ? "Change to ℉" : "Change to ℃"} </button>
+        </div>
+
+        <div className="contenidoinfo">
           <h2>{weather?.name}, {weather?.sys.country}</h2>
-      <div>      
-      <div>
-        <img className="clouds"
-          src={weather && `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`}  />
-      </div>     
-      <section className="info">
-        <h3>"{weather?.weather[0].description}"</h3>
-        <ul>
-          <li><span>Wind Speed</span>
-          {weather?.wind.speed} </li>
-          <li>Clouds<span>
-            {weather?.clouds.all}</span>
-           </li>
-          <li>Pressure<span>
-            {weather?.main.pressure}</span>
-           </li>
-        </ul>
-       </section>
-      </div>
-      <h2 className="temperature">{isCelsius ? `${temp?.celsius} ℃`: `${temp?.farenheit} ℉`}</h2>
-      <button onClick={handleChangeTemp}>{isCelsius ? "Change to ℉" : "Change to ℃"} </button>
+          <section className="info">
+            <h3>"{weather?.weather[0].description}"</h3>
+            <ul>
+              <li><span>Wind Speed</span>
+              {weather?.wind.speed} </li>
+              <li>Clouds<span>
+                {weather?.clouds.all}</span>
+              </li>
+              <li>Pressure<span>
+                {weather?.main.pressure}</span>
+              </li>
+            </ul>
+          </section>
+        </div>
+
       </article>
   )
 }
